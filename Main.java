e47950c3b97540447f440b7ca542e9dcf0f92d8f
justinryansmith10-cmd <justinryansmith10@gmail.com>
2025-11-05 21:53:26 -0500

    Generates a window of the game client.

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
+import javax.swing.JFrame;
+
 /**
  * Runs the game on the console
  */
 public class Main {
 
 	public static void main(String[] args) {
+
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Frogger(Java)");
+
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+
+		window.pack();
+
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
 
 
 	}
